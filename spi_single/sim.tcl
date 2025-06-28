7caf9cda727ccfeda4e3e4f94cf6471d7817f3e0
smf <smf616@outlook.com>
2025-06-28 23:17:29 +0800

    update sim.tcl

--- spi_single/sim.tcl
+++ spi_single/sim.tcl
@@ -1,8 +1,13 @@
 if ![file isdirectory ./work] { vlib ./work }
 vmap work ./work
 vlog spi_master.sv
-vlog spi_slave_single.sv
-vlog xlib_xyz_fifoa.v
+vlog spi_slave.sv
+vlog xlib_xyz_fifo.v
+vlog xc_sync_fifo_rec.v
+vlog crc.v
+vlog packet_generate_tb.sv
+vlog spi_packet_rx.sv
+vlog spi_packet_tx.sv
 vlog tb.sv +libext+.v +libext+.sv
  vsim work.tb  -L altera_mf_ver -voptargs=+acc
  do wave.do
